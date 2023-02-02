8a5db39df7f974b18899f158c281617d18f6f940
Dayane <dayanecarolina.oliveira@gmail.com>
2023-02-02 00:06:40 -0300

    test: Get specific fruit and Post fruit

--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -1,7 +1,6 @@
 import supertest from "supertest";
 import app from "../src/index";
 import fruits from "data/fruits";
-import { Fruit } from "repositories/fruits-repository";
 
 const api = supertest(app);
 
@@ -9,7 +8,7 @@ beforeEach (()=>{
     fruits.length=0;
 })
 
-describe('/fruits',()=>{
+describe('GET:/fruits',()=>{
     it('Deve retornar a lista de todas as frutas',async()=>{
         fruits.push({   
             id: 1,
@@ -35,4 +34,77 @@ describe('/fruits',()=>{
             ])
         )
     })
+
+    it('Deve retornar um array vazio',async()=>{
+        
+        const resultado = await api.get('/fruits');
+        expect(resultado.status).toBe(200);
+        expect(resultado.body).toEqual(
+            expect.arrayContaining([])
+        )
+    })
+})
+
+describe('GET:/fruits/:id',()=>{
+    it('Deve retornar o objeto fruit de id solicitado',async ()=>{
+        fruits.push({   
+            id: 1,
+            name: "Banana",
+            price: 1000
+          },
+          {   
+            id: 2,
+            name: "Apple",
+            price: 2000
+          });
+        const resultado = await api.get('/fruits/2');
+        expect(resultado.body.id).toBe(2);
+    })
+
+    it('Deve retornar status 404',async ()=> {
+        fruits.push({   
+            id: 1,
+            name: "Banana",
+            price: 1000
+          },
+          {   
+            id: 2,
+            name: "Apple",
+            price: 2000
+          });
+          const resultado = await api.get('/fruits/3');
+          console.log(resultado);
+          expect(resultado.status).toBe(404);
+    })
+
+})
+
+describe('POST:/fruits',()=>{
+    it('Deve retornar status 201',async ()=>{
+        const resultado = await api.post('/fruits').send({
+            name: 'Abacaxi',
+            price:2500
+        });
+        expect(resultado.status).toBe(201);
+    })
+
+    it('Deve retornar status 422',async ()=>{
+        const resultado = await api.post('/fruits').send({
+            name: 'Abacaxi'
+        });
+        expect(resultado.status).toBe(422);
+    })
+    it('Deve retornar status 409',async ()=>{
+        fruits.push({   
+            id: 1,
+            name: "Abacaxi",
+            price: 1000
+          });
+        const resultado = await api.post('/fruits').send({
+            name: 'Abacaxi',
+            price:2500
+        });
+        expect(resultado.status).toBe(409);
+    })
+
 })
